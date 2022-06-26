022fdab8e3bed56e212f04895fe986eff5087f20
John Nguyen <john.nguyen@kreuzwerker.de>
2022-06-26 18:08:50 +0200

    Change to Action buttons and added Google Auth

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
   <h1 text-center>Consule.me</h1>
-  <div grid grid-cols-6 gap-5 p-5 h-screen>
-    <NuxtLayout name="sidebar">
-      <DragObject />
-    </NuxtLayout>
+  <div grid grid-cols-12 gap-5 p-5 h-screen>
+    <NuxtLayout name="sidebar" />
     <NuxtLayout name="base">
       <Dashboard />
     </NuxtLayout>
